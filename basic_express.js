548f6059918f3da58922a59b13f0b3992dffdf93
acaron1 <90399705+acaron1@users.noreply.github.com>
2021-09-16 13:39:46 -0400

    Update basic_express.js

--- basic_express.js
+++ basic_express.js
@@ -3,6 +3,9 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
+app.get('/about', function (req, res) {
+  res.send('About');
+});
 var server = app.listen(8000, function () {
   var host = server.address().address;
   var port = server.address().port;
