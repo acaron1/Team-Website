017d16311437b4d6e379b58bcdda7e541218bcdd
SteelRacer88 <aidan2023382@live.ytech.edu>
2021-09-22 14:54:05 -0400

    EJS

--- basic_express.js
+++ basic_express.js
@@ -9,26 +9,26 @@ app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
 // Set Templating Engine
 app.use(expressLayouts)
-app.set('layout', './layouts/full-width')
+app.set('layout', './layout/layout')
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.sendFile('/public/home.html', {root: __dirname});
+  res.render('home');
 });
 app.get('/about', function (req, res) {
-  res.sendFile('/public/about.html', {root: __dirname});
+  res.render('about');
 });
 app.get('/sammy', function (req, res) {
-  res.sendFile('/public/Sammy.html', {root: __dirname});
+  res.render('Sammy');
 });
 app.get('/aidan', function (req, res) {
-  res.render('aidan');
+  res.render('Aidan');
 });
 app.get('/abby', function (req, res) {
-  res.sendFile('/public/Abby.html', {root: __dirname});
+  res.render('Abby');
 });
 app.get('/jason', function (req, res) {
-  res.sendFile('/public/Jason.html', {root: __dirname});
+  res.render('Jason');
 });
 var server = app.listen(8000, function () {
   var host = server.address().address;
